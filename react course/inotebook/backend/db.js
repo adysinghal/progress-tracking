d31b3d845d34fe44827229d91a3996ccc9172706
Aditya Singhal <singhal.adityaa@gmail.com>
2024-04-05 12:46:55 +0530

    iNotebook

--- react course/inotebook/backend/db.js
+++ react course/inotebook/backend/db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-const mongoURI  = "mongodb://localhost:27017";
+// const mongoURI  = "mongodb://localhost:27017";
+const mongoURI  = "mongodb://0.0.0.0:27017/";
+// const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI,()=>{
--- react course/inotebook/backend/routes/auth.js
+++ react course/inotebook/backend/routes/auth.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
+const User = require('../models/User');
+// const user = User;
 
-router.get('/', (req, res) => {
-    obj = {
-        a: 'theo',
-        number: 34
-    }
-    res.json(obj);
+// create a user using: POST "/api/auth/". Doesn't require Auth
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.send(req.body);
 })
 
 module.exports = router
